65fccf6e46d8fdd8b6abeb1bd040aaddf045dfa9
Kevin Peters <kwpeters@ra.rockwell.com>
2021-02-03 11:57:56 -0500

    Added more examples.

---
+++ FSharpExamples.Test/Entity.fs
@@ -0,0 +1,66 @@
+module EntityTests
+
+open System
+open Xunit
+
+//------------------------------------------------------------------------------
+
+
+type ContactId =
+    | ContactId of string
+
+
+type PhoneNumber =
+    | PhoneNumber of string
+
+
+[<NoEquality; NoComparison>]
+type Contact =
+    { ContactId: ContactId
+      PhoneNumber: PhoneNumber }
+
+
+[<Fact>]
+let ``Entity equality`` () =
+    let contact1: Contact =
+        { ContactId = ContactId "001"
+          PhoneNumber = PhoneNumber "123" }
+
+    let contact2: Contact =
+        { ContactId = ContactId "002"
+          PhoneNumber = PhoneNumber "456" }
+
+    // if contact1 = contact2 then         // Error: The type Contact does not support equality.
+    //     printfn "Cannot compare the entities directly."
+
+    // The ID fields must be compared explicitly.
+    Assert.NotEqual(contact1.ContactId, contact2.ContactId)
+
+
+//------------------------------------------------------------------------------
+
+
+[<NoEquality; NoComparison>]
+type Contact2 =
+    { ContactId1: string
+      ContactId2: string
+      PhoneNumber: PhoneNumber }
+    with
+    member this.Key =
+        (this.ContactId1, this.ContactId2)
+
+
+[<Fact>]
+let ``Entity identity - multiple fields`` () =
+    let contact1: Contact2 =
+        { ContactId1 = "foo"
+          ContactId2 = "bar"
+          PhoneNumber = PhoneNumber "123" }
+
+    let contact2: Contact2 =
+        { ContactId1 = "foo"
+          ContactId2 = "bar"
+          PhoneNumber = PhoneNumber "456" }
+
+    Assert.Equal(contact1.Key, contact2.Key)
+
--- FSharpExamples.Test/Record.fs
+++ FSharpExamples.Test/Record.fs
@@ -24,3 +24,17 @@ let ``A basic record type example`` () =
     let { FirstName = firstName } = fred
     Assert.Equal("Fred", firstName)
 
+
+//------------------------------------------------------------------------------
+
+[<Fact>]
+let ``Copying records`` () =
+    let person1 =
+        { FirstName = "Fred"
+          MiddleName = Some "Willard"
+          LastName = "Flintstone" }
+
+    let person2 = {person1 with MiddleName = None}
+    Assert.Equal("Fred", person2.FirstName)
+    Assert.Equal(None, person2.MiddleName)
+    Assert.Equal("Flintstone", person2.LastName)
